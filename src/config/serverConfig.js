420c21447fd215b1da98f5438dbdb3316938f733
Rohit <rohitsrivas83@gmail.com>
2024-07-28 21:17:50 +0530

    force

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -1,6 +1,5 @@
-const dotenv = require("dotenv");
+const dotenv =require("dotenv");
 dotenv.config();
-
-module.exports = {
-    PORT: process.env.PORT || 3000 // Default to port 3000 if PORT is not set
-};
+module.exports={
+    PORT: process.env.PORT
+}
