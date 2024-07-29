e9b6999a47f3934d11f2fe71db45e3d65cf8db8b
Rohit <rohitsrivas83@gmail.com>
2024-07-29 20:16:49 +0530

    Added routers to compltee create api

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+
 const { PORT } = require('./config/serverConfig.js');
-const CityRepository = require("./repository/city-repository.js");
+const ApiRoutes = require('./routes/index.js');
 
 const setUpAndStartServer = async () => {
     const app = express();
@@ -9,17 +10,14 @@ const setUpAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
+
+    app.use('/api',ApiRoutes); 
+
     const port = PORT || 3000; // Default to port 3000 if PORT is not set
     console.log(`Attempting to start server on port: ${port}`);
 
     app.listen(port, async () => {
         console.log(`Server started at ${port}`);
-        const repo = new CityRepository();
-        try {
-            await repo.deleteCity(1);
-        } catch (error) {
-            console.error('Error deleting city:', error);
-        }
     });
 };
 
